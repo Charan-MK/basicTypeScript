dc2d331b64527a28b0b6d826f44f7de9f813ff40
Charan-MK <121428200+Charan-MK@users.noreply.github.com>
2025-04-16 12:26:01 +0530

    intermediate types enums, alias, interface and union types

--- src/index.js
+++ src/index.js
@@ -40,3 +40,79 @@ const nameAgeMap = {}; // Index Signatures: can be used for objects without a de
 nameAgeMap.bob = 26;
 nameAgeMap.alice = 21;
 console.log('example of an index signature: ', nameAgeMap);
+/**
+ * Enums - Numeric enums
+ */
+var directions;
+(function (directions) {
+    directions[directions["East"] = 0] = "East";
+    directions[directions["West"] = 1] = "West";
+    directions[directions["North"] = 2] = "North";
+    directions[directions["South"] = 3] = "South";
+})(directions || (directions = {}));
+;
+console.log('an enum of direction: ', directions);
+console.log('First direction is: ', directions.East, 'second is: ', directions.West);
+// assigned enums
+var grades;
+(function (grades) {
+    grades[grades["S"] = 100] = "S";
+    grades[grades["A"] = 90] = "A";
+    grades[grades["B"] = 75] = "B";
+    grades[grades["C"] = 60] = "C";
+    grades[grades["D"] = 40] = "D";
+})(grades || (grades = {}));
+;
+// initialised enums
+var sampleEnum;
+(function (sampleEnum) {
+    sampleEnum[sampleEnum["first"] = 1] = "first";
+    sampleEnum[sampleEnum["second"] = 2] = "second";
+    sampleEnum[sampleEnum["third"] = 3] = "third";
+})(sampleEnum || (sampleEnum = {}));
+;
+console.log('Assigned enums: ', grades, 'and initialized enums: ', sampleEnum);
+console.log('accessing grade enum: ', grades.S);
+/**
+ * String enums
+ */
+var cardinalDirections;
+(function (cardinalDirections) {
+    cardinalDirections["North"] = "North";
+    cardinalDirections["East"] = "East";
+    cardinalDirections["South"] = "South";
+    cardinalDirections["West"] = "West";
+})(cardinalDirections || (cardinalDirections = {}));
+;
+console.log('string enums: ', cardinalDirections);
+const firstCar = {
+    year: 2014,
+    bodyType: 'hatchback',
+    model: 'swift',
+    type: 'suzuki',
+};
+console.log('firstCar defined from types: ', firstCar);
+const secondCar = {
+    year: 2022,
+    bodyType: 'sedan',
+    model: 'dzire',
+    type: 'suzuki'
+};
+console.log('secondCar defined with Car type: ', secondCar);
+;
+const rectangle = {
+    heightInCm: 12,
+    widthInCm: 8,
+};
+console.log('Area of rectangle is: ', rectangle.heightInCm * rectangle.widthInCm, 'square centimeter');
+const firstRes = { success: true };
+const secondRes = { error: 'service temparoriy down' };
+console.log('an exampleof union in type alas: ', firstRes, secondRes);
+/**
+ * Unuon types
+ */
+function printCode(code) {
+    console.log('thsis is a code: ', code);
+}
+printCode(404);
+printCode('401');
--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,112 @@ console.log('an object with optional property owner: ', bike);
 const nameAgeMap: { [index: string]: number} = {}; // Index Signatures: can be used for objects without a defined list of properties.
 nameAgeMap.bob = 26; nameAgeMap.alice = 21;
 console.log('example of an index signature: ', nameAgeMap);
+
+/**
+ * Enums - Numeric enums
+ */
+
+enum directions {
+    East, West, North, South,
+};
+console.log('an enum of direction: ', directions);
+console.log('First direction is: ', directions.East, 'second is: ', directions.West);
+
+// assigned enums
+enum grades {
+    S = 100,
+    A = 90,
+    B = 75,
+    C = 60,
+    D = 40,
+};
+
+// initialised enums
+enum sampleEnum {
+    first = 1,
+    second,
+    third,
+};
+console.log('Assigned enums: ', grades, 'and initialized enums: ', sampleEnum);
+console.log('accessing grade enum: ', grades.S);
+
+/**
+ * String enums
+ */
+enum cardinalDirections {
+    North = 'North',
+    East = "East",
+    South = "South",
+    West = "West",
+};
+console.log('string enums: ', cardinalDirections);
+
+/**
+ * Aliases and Interfaces
+ */
+
+// Aliases can be used to define types separately
+
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+type BodyType = string;
+
+const firstCar: { year: CarYear, bodyType: BodyType, model: CarModel, type: CarType } = {
+    year: 2014,
+    bodyType: 'hatchback',
+    model: 'swift',
+    type: 'suzuki',
+};
+console.log('firstCar defined from types: ', firstCar);
+
+type Car = {
+    year: CarYear,
+    model: CarModel,
+    bodyType: BodyType,
+    type: CarType,
+};
+
+const secondCar: Car = {
+    year: 2022,
+    bodyType: 'sedan',
+    model: 'dzire',
+    type: 'suzuki'
+}
+console.log('secondCar defined with Car type: ', secondCar);
+
+// interfaces
+interface Rectangle {
+    heightInCm: number,
+    widthInCm: number,
+};
+
+const rectangle: Rectangle = {
+    heightInCm: 12,
+    widthInCm: 8,
+}
+console.log('Area of rectangle is: ', rectangle.heightInCm * rectangle.widthInCm, 'square centimeter');
+
+/**
+ * Key differences between type Alias and Interface are:
+ * 1. Alias can represent a verity of types such as promitive, arrays, tuples, unions or intersection
+ * 2. Alias support unions(|) and intersections (&): 
+ */
+
+type Cordinate = [number, number];
+type ApiResponse = { success: boolean } | { error: string };
+const firstRes = { success: true };
+const secondRes = { error: 'service temparoriy down' };
+console.log('an exampleof union in type alas: ', firstRes, secondRes);
+
+/**
+ * Union types
+ */
+function printCode(code: string | number) {
+    console.log('thsis is a code: ', code);
+}
+
+printCode(404);
+printCode('401');
+// Union of types can result in error while performing some actions on the declared variable
+// eg., in the above code, code.toUpperCase() can result in error when the code is of type string
